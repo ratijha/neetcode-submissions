8e800967be8bdc8e64f9106e92cdac2101276e33
ratijha <raticse@gmail.com>
2026-07-20 18:50:49 +0530

    Update submission-0.py

--- Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -11,3 +11,18 @@ class Solution:
                 right = right -1 
             elif sum < target:
                 left = left + 1
+
+# if the array is unsorted 
+def twoSumUnsorted(numbers, target):
+    # Map to store: key = number_needed, value = its_0_indexed_position
+    seen = {}
+    
+    for i, num in enumerate(numbers):
+        complement = target - num
+        
+        if complement in seen:
+            # Found the pair! Convert to 1-indexed before returning
+            return [seen[complement] + 1, i + 1]
+        
+        # Store current number's index
+        seen[num] = i
